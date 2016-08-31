a8609cfbf8e0f53762ffe5a29ea9c443551c5f44
Andrew Tandiawan & Elkana Lumbantobing <pair+atandiawan.elkana-ltobing@hacktiv8.com>
2016-08-31 19:48:36 +0700

    Regex 2/5

--- regular_expressions.js
+++ regular_expressions.js
@@ -1,8 +1,10 @@
 "use strict"
 // Determine whether a string contains a nomor KTP
 function has_ktp(string) {
-
+  let result = (/[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]$/).test(string);
+  return result
 }
+
 
 console.log("has_ktp returns true if it has what looks like a nomor KTP")
 console.log(has_ktp("please don't share this: 234-60-1422") == true)
@@ -12,6 +14,13 @@ console.log(has_ktp("please confirm your identity: XXX-XX-1422") == false)
 
 // Return the Social Security number from a string.
 function grab_ktp(string) {
+  let index = string.search(/[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]$/)
+  if (index != -1){
+    let result = string.substr(index,11)
+    return result;
+  } else {
+    return null;
+  }
 
 }
 
@@ -21,45 +30,45 @@ console.log(grab_ktp("please don't share this: 234-60-1422") == "234-60-1422")
 console.log("grab_ssn returns nil if it doesn't have a nomor KTP")
 console.log(grab_ktp("please confirm your identity: XXX-XX-1422") == null)
 
-// Return all of the Social Security numbers from a string.
-function grab_all_nomor_ktp(string) {
-
-}
-
-console.log("grab_all_nomor_ktp returns all nomor KTP if the string has any nomor KTP")
-console.log(grab_all_nomor_ktp("234-60-1422, 350-80-0744, 013-60-8762"))
-
-// return ["234-60-1422", "350-80-0744", "013-60-8762"])
-
-console.log("grab_all_nomor_ktp returns an empty Array if it doesn't have any nomor KTP")
-console.log(grab_all_nomor_ktp("please confirm your identity: XXX-XX-1422"))
-// return []
-
-
-// Obfuscate all of the nomor KTP in a string. Example: XXX-XX-4430.
-function hide_all_nomor_ktp(string) {
-
-}
-
-console.log("hide_all_nomor_ktp obfuscates any nomor KTP in the string")
-console.log(hide_all_nomor_ktp("234-60-1422, 350-80-0744, 013-60-8762"))
-
-// "XXX-XX-1422, XXX-XX-0744, XXX-XX-8762"
-
-console.log("hide_all_nomor_ktp does not alter a string without nomor KTP in it")
-var string = "please confirm your identity: XXX-XX-1422"
-console.log(hide_all_nomor_ktp(string) == string)
-
-
-// Ensure all of the Social Security numbers use dashes for delimiters.
-// Example: 480.01.4430 and 480014430 would both be 480-01-4430.
-function format_nomor(string) {
-
-}
-
-console.log("format_nomor finds and reformat any nomor KTP in the string")
-console.log(format_nomor("234601422, 350.80.0744, 013-60-8762") == "234-60-1422, 350-80-0744, 013-60-8762")
-
-console.log("format_nomor does not alter a string without nomor KTP in it")
-string = "please confirm your identity: 44211422"
-console.log(format_nomor(string) == string)
+// // Return all of the Social Security numbers from a string.
+// function grab_all_nomor_ktp(string) {
+//
+// }
+//
+// console.log("grab_all_nomor_ktp returns all nomor KTP if the string has any nomor KTP")
+// console.log(grab_all_nomor_ktp("234-60-1422, 350-80-0744, 013-60-8762"))
+//
+// // return ["234-60-1422", "350-80-0744", "013-60-8762"])
+//
+// console.log("grab_all_nomor_ktp returns an empty Array if it doesn't have any nomor KTP")
+// console.log(grab_all_nomor_ktp("please confirm your identity: XXX-XX-1422"))
+// // return []
+//
+//
+// // Obfuscate all of the nomor KTP in a string. Example: XXX-XX-4430.
+// function hide_all_nomor_ktp(string) {
+//
+// }
+//
+// console.log("hide_all_nomor_ktp obfuscates any nomor KTP in the string")
+// console.log(hide_all_nomor_ktp("234-60-1422, 350-80-0744, 013-60-8762"))
+//
+// // "XXX-XX-1422, XXX-XX-0744, XXX-XX-8762"
+//
+// console.log("hide_all_nomor_ktp does not alter a string without nomor KTP in it")
+// var string = "please confirm your identity: XXX-XX-1422"
+// console.log(hide_all_nomor_ktp(string) == string)
+//
+//
+// // Ensure all of the Social Security numbers use dashes for delimiters.
+// // Example: 480.01.4430 and 480014430 would both be 480-01-4430.
+// function format_nomor(string) {
+//
+// }
+//
+// console.log("format_nomor finds and reformat any nomor KTP in the string")
+// console.log(format_nomor("234601422, 350.80.0744, 013-60-8762") == "234-60-1422, 350-80-0744, 013-60-8762")
+//
+// console.log("format_nomor does not alter a string without nomor KTP in it")
+// string = "please confirm your identity: 44211422"
+// console.log(format_nomor(string) == string)
