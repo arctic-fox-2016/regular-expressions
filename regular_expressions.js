024a2ae0d06df28ac3d55e7806bc21fe82f7b6f5
Septhianto Diga & Fadhli Ramadhani Arislan <pair+digachandra.fadhlirams@hacktiv8.com>
2016-08-31 20:22:14 +0700

    regular_expressions

--- regular_expressions.js
+++ regular_expressions.js
@@ -1,9 +1,10 @@
 "use strict"
 // Determine whether a string contains a nomor KTP
 function has_ktp(string) {
-
+  return /[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]$/i.test(string);
 }
 
+console.log("\n\nCase 1:");
 console.log("has_ktp returns true if it has what looks like a nomor KTP")
 console.log(has_ktp("please don't share this: 234-60-1422") == true)
 
@@ -12,9 +13,10 @@ console.log(has_ktp("please confirm your identity: XXX-XX-1422") == false)
 
 // Return the Social Security number from a string.
 function grab_ktp(string) {
-
+  return string.match(/[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]$/i);
 }
 
+console.log("\n\nCase 2:");
 console.log("grab_ktp returns an nomor KTP if the string has an nomor KTP")
 console.log(grab_ktp("please don't share this: 234-60-1422") == "234-60-1422")
 
@@ -23,9 +25,14 @@ console.log(grab_ktp("please confirm your identity: XXX-XX-1422") == null)
 
 // Return all of the Social Security numbers from a string.
 function grab_all_nomor_ktp(string) {
+  var result_regex = [];
+  var test_regex = string.match(/[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]/g);
 
+  if(test_regex) result_regex = test_regex;
+  return result_regex;
 }
 
+console.log("\n\nCase 3:");
 console.log("grab_all_nomor_ktp returns all nomor KTP if the string has any nomor KTP")
 console.log(grab_all_nomor_ktp("234-60-1422, 350-80-0744, 013-60-8762"))
 
@@ -38,9 +45,24 @@ console.log(grab_all_nomor_ktp("please confirm your identity: XXX-XX-1422"))
 
 // Obfuscate all of the nomor KTP in a string. Example: XXX-XX-4430.
 function hide_all_nomor_ktp(string) {
+  var result_regex = [];
+  var test_regex = string.match(/[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]/g);
 
+  if(test_regex){
+    var temp_regex = [];
+    for(var idx = 0; idx < test_regex.length; idx++){
+      temp_regex[idx] = "";
+      for(var idy = 0; idy < test_regex[idx].length; idy++){
+        if(idy < 3 || (idy >=4 && idy <=5)) temp_regex[idx] += "X";
+        else temp_regex[idx] += test_regex[idx][idy];
+      }
+    }
+    result_regex = temp_regex;
+  }
+  return result_regex;
 }
 
+console.log("\n\nCase 4:");
 console.log("hide_all_nomor_ktp obfuscates any nomor KTP in the string")
 console.log(hide_all_nomor_ktp("234-60-1422, 350-80-0744, 013-60-8762"))
 
@@ -54,9 +76,31 @@ console.log(hide_all_nomor_ktp(string) == string)
 // Ensure all of the Social Security numbers use dashes for delimiters.
 // Example: 480.01.4430 and 480014430 would both be 480-01-4430.
 function format_nomor(string) {
+  var result_regex = [];
+  var test_regex = string.match(/([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|[0-9][0-9][0-9].[0-9][0-9].[0-9][0-9][0-9][0-9]|[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9])/g);
 
+  var new_string = "";
+  if(test_regex){
+    for(var idx = 0; idx < test_regex.length; idx++){
+
+      if(new_string) new_string += ", ";
+
+      if(idx == 0){
+        new_string += test_regex[idx][0] + test_regex[idx][1] + test_regex[idx][2] + "-" + test_regex[idx][3] + test_regex[idx][4] + "-" + test_regex[idx][5] + test_regex[idx][6] + test_regex[idx][7] + test_regex[idx][8];
+      } else if(idx == 1){
+        new_string += test_regex[idx][0] + test_regex[idx][1] + test_regex[idx][2] + "-" + test_regex[idx][4] + test_regex[idx][5] + "-" + test_regex[idx][7] + test_regex[idx][8] + test_regex[idx][9] + test_regex[idx][10];
+      } else if(idx == 2){
+        new_string += test_regex[idx];
+      }
+    }
+    console.log(new_string);
+
+    return new_string;
+  }
+  return false;
 }
 
+console.log("\n\nCase 5:");
 console.log("format_nomor finds and reformat any nomor KTP in the string")
 console.log(format_nomor("234601422, 350.80.0744, 013-60-8762") == "234-60-1422, 350-80-0744, 013-60-8762")
 
